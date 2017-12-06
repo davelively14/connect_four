a9765d382360e42229274187143f00c2671e503d
Dave Lively <davelively14@gmail.com>
2017-12-05 23:00:04 -0500

    Added drop_piece/1 and reset/0
    drop_piece/1 can likely be refactored, but it works for now. reset/0
    just resets the state.

--- lib/otp/game_server.ex
+++ lib/otp/game_server.ex
@@ -1,6 +1,8 @@
 defmodule ConnectFour.GameServer do
   use GenServer
 
+  @default_state %{board: %{a: [], b: [], c: [], d: [], e: [], f: [], g: []}, current_player: 0}
+
   #######
   # API #
   #######
@@ -11,6 +13,14 @@ defmodule ConnectFour.GameServer do
 
   def get_state do
     GenServer.call(__MODULE__, :get_state)
+  end
+
+  def drop_piece(row) do
+    GenServer.call(__MODULE__, {:drop_piece, row})
+  end
+
+  def reset do
+    GenServer.call(__MODULE__, :reset)
   end
 
   #############
@@ -25,4 +35,29 @@ defmodule ConnectFour.GameServer do
   def handle_call(:get_state, _from, state) do
     {:reply, state, state}
   end
+
+  def handle_call({:drop_piece, row}, _from, state) do
+    if length(state.board[row]) < 6 do
+      {_, new_board} =
+        Map.get_and_update(state.board,
+                           row,
+                           fn current_value ->
+                             {current_value, [state.current_player | state.board[row]]}
+                           end)
+      new_state = %{board: new_board, current_player: advance_player(state.current_player)}
+      {:reply, :ok, new_state}
+    else
+      {:reply, {:error, "Row is full"}, state}
+    end
+  end
+
+  def handle_call(:reset, _from, _state) do
+    {:reply, :ok, @default_state}
+  end
+
+  #####################
+  # Private Functions #
+  #####################
+
+  defp advance_player(current_player), do: rem(current_player + 1, 2)
 end
